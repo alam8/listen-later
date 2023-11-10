69ad60c5d2e26f604c7ad3b0bcc9badf182b6189
Garrett Brenner <garrebrenna@gmail.com>
2023-11-10 11:50:53 -0800

    Added pylint config and relaxed python version

--- listen_later/model/collection.py
+++ listen_later/model/collection.py
@@ -2,14 +2,16 @@ import datetime as dt
 
 from marshmallow import Schema, fields, post_load
 
-class Collection(object):
-    def __init__(self, id, name ):
+
+class Collection:
+    def __init__(self, id, name):
         self.id = id
         self.name = name
         self.date_added = dt.datetime.now()
 
     def __repr__(self):
         return '<Collection(id={self.id!r},name={self.name!r})>'.format(self=self)
+
 
 class CollectionSchema(Schema):
     id = fields.Int()
