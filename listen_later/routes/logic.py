768ebef3aca48ac48d7457bf355bb20b74e8b04b
Andrew Lam <lamandrew2000@gmail.com>
2023-12-15 17:36:24 -0800

    Finish add_item_to_collection()

--- listen_later/routes/logic.py
+++ listen_later/routes/logic.py
@@ -5,12 +5,13 @@ from google.cloud.firestore_v1.base_query import FieldFilter
 
 @app.route("/collections/<string:collection_pk>/<string:item_pk>", methods=['PUT', 'POST'])
 def add_item_to_collection(collection_pk, item_pk):
-    # TODO: Fix error handling if collection/item not found
     collection_ref = collection.get_collection_ref(collection_pk)
     collection_doc = collection_ref.get()
 
     if not collection_doc.exists:
         return not_found_error(COLLECTION_TYPE, collection_pk)
+    elif collection_pk == ALL_COLLECTION_ID:
+        return {"errors": "Items cannot be added to All collection"}, 403
     
     item_ref = item.get_item_ref(item_pk)
     item_doc = item_ref.get()
@@ -18,17 +19,20 @@ def add_item_to_collection(collection_pk, item_pk):
     if not item_doc.exists:
         return not_found_error(ITEM_TYPE, item_pk)
 
-    # TODO: Fix collections sub-fbc of item not being copied correctly (maybe convert to map instead?)
-    collection_ref.collection(ITEMS).document(item_pk).set(item.get_item(item_pk))
+    added_item_ref = collection_ref.collection(ITEMS).document(item_pk)
+    added_item_ref.set(item_doc.to_dict())
+
+    item_collections_ref = item_ref.collection(COLLECTIONS).stream()
+    for item_collection_ref in item_collections_ref:
+        added_item_ref.collection(COLLECTIONS).document(item_collection_ref.id).set(item_collection_ref.to_dict())
 
     items_query = db.collection_group(ITEMS).where(
         filter=FieldFilter("id", "==", item_pk)
     ).stream()
+    for queried_item_doc in items_query:
+        queried_item_doc.reference.collection(COLLECTIONS).document(collection_pk).set(collection_doc.to_dict())
 
-    for item_doc in items_query:
-        item_doc.reference.collection(COLLECTIONS).document(collection_pk).set(collection_doc.to_dict())
-
-    return f'Added Item(id={item_pk} to Collection(id={collection_pk}) successfully', 200
+    return f'Added Item(id={item_pk}) to Collection(id={collection_pk}) successfully', 200
 
 @app.route("/collections/<string:collection_pk>/<string:item_pk>", methods=['DELETE'])
 def remove_item_from_collection(collection_pk, item_pk):
